d12711ca60cd4bfde78954d28f607d412bf894a1
Blake Abernathy <minerabacuss@gmail.com>
2024-05-18 18:51:55 -0500

    Starting over

---
+++ build/CMakeFiles/leibniz.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for leibniz.
+# This may be replaced when dependencies are built.
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,63 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
-#include <iostream>
-#include "AL/al.h"
-#include "AL/alc.h"
 
 int main()
 {
-    if (!glfwInit())
-    {
-        std::cout << "FAIL\n";
-        glfwTerminate();
-    }
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Title", NULL, NULL);
-    if (!window)
-    {
-        std::cout << "FAIL\n";
+    // Initialize GLFW
+    if (!glfwInit())
         glfwTerminate();
-    }
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Title", NULL, NULL);
+    if (!window)
+        glfwTerminate();
 
     glfwMakeContextCurrent(window);
 
-    ALCdevice *alcDevice = alcOpenDevice(NULL);
-    ALCcontext *alcContext;
+    // Connect ImGui
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460 core");
 
-    if (alcDevice)
-    {
-        alcContext = alcCreateContext(alcDevice, NULL);
-        alcMakeContextCurrent(alcContext);
-    }
-
-    // TODO: Get sound to play
-
+    // Loop
     while (!glfwWindowShouldClose(window))
     {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Text("This is some useful text.");
+        ImGui::End();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
-
-        glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
+    glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
 }
+
